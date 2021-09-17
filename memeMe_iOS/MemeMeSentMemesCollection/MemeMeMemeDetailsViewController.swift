0f9458ebb78c8d359d40c15c29c73556bb40aabf
Andrew Bui <andrew.bui.au@gmail.com>
2021-09-17 20:51:24 +1000

    added view elements to deatils view

--- memeMe_iOS/MemeMeSentMemesCollection/MemeMeMemeDetailsViewController.swift
+++ memeMe_iOS/MemeMeSentMemesCollection/MemeMeMemeDetailsViewController.swift
@@ -15,7 +15,35 @@ class MemeMeMemeDetailsViewController: UIViewController {
     private var memeMeDetailsToolbar: UIToolbar = {
         let uiToolbar = UIToolbar()
         uiToolbar.translatesAutoresizingMaskIntoConstraints = false
+        uiToolbar.barTintColor = .black
+        uiToolbar.sizeToFit()
         return uiToolbar
+    }()
+    
+    private lazy var memeTopLabel: UILabel = {
+        createMemeUILabel()
+    }()
+    
+    private lazy var memeBottomLabel: UILabel = {
+        createMemeUILabel()
+    }()
+    
+    func createMemeUILabel() ->  UILabel {
+        let uiLabel = UILabel()
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        uiLabel.textAlignment = .center
+        uiLabel.textColor = .white
+        uiLabel.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)
+        uiLabel.numberOfLines = 0
+        return uiLabel
+    }
+    
+    private var memeImageOriginal: UIImageView = {
+        let uiImageView = UIImageView()
+        uiImageView.translatesAutoresizingMaskIntoConstraints = false
+        uiImageView.backgroundColor = .red
+        uiImageView.contentMode = .scaleAspectFit
+        return uiImageView
     }()
     
     init(memeDetails: Meme) {
@@ -30,6 +58,28 @@ class MemeMeMemeDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        print(memeDetails.bottomText)
+        
+        memeTopLabel.text = memeDetails.topText
+        
+        // Details Toolbar
+        view.addSubview(memeMeDetailsToolbar)
+        view.addSubview(memeTopLabel)
+        view.addSubview(memeImageOriginal)
+        view.addSubview(memeBottomLabel)
+        
+        // Details Toolbar Constraints
+        NSLayoutConstraint.activate([
+            memeMeDetailsToolbar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0.0),
+            memeMeDetailsToolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            memeMeDetailsToolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            memeMeDetailsToolbar.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memeTopLabel.topAnchor.constraint(equalTo: memeMeDetailsToolbar.bottomAnchor, constant: 25.0),
+            memeTopLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10.0),
+            memeTopLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10.0),
+            memeTopLabel.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
     }
 }
